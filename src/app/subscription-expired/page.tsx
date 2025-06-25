15d6f4f5b413867e8791fa844d5590e40cac6b6b
Althaf Hussain <althafhussainpc22@gmail.com>
2025-06-25 07:00:33 +0530

    fix

--- src/app/subscription-expired/page.tsx
+++ src/app/subscription-expired/page.tsx
@@ -1,19 +1,25 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
 import "./SubscriptionExpired.scss";
 
 const SubscriptionExpired = () => {
   return (
     <div className="subscription-expired-container">
       <div className="subscription-card">
-        <img
+        <Image
           src="/gifs/expired.gif"
           alt="Subscription Expired"
           className="expired-gif"
+          width={300}
+          height={300}
+          priority // Optional: Optimizes for LCP
         />
         <h2 className="expired-title">Oops! Subscription Expired</h2>
         <p className="expired-text">
-          Your access has expired. Everything's still here — just waiting for
-          you to come back.
+          Your access has expired. Everything&apos;s still here — just waiting
+          for you to come back.
         </p>
       </div>
     </div>
